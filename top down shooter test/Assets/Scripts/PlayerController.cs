b3b4c852ad673d3a3e591ca24fa07ff847966bd4
LeviathansDen <levi-woollven@hotmail.co.uk>
2025-04-01 12:57:56 +0100

    Added aiming

--- top down shooter test/Assets/Scripts/PlayerController.cs
+++ top down shooter test/Assets/Scripts/PlayerController.cs
@@ -8,25 +8,48 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D player;
     private Animator playerAnimator;
     private GameObject legPivot;
+    private GameObject armPivot;
     private GameObject head;
+    private GameObject unarmed;
+    private bool unarmedEquipped = true;
+    private GameObject pistolObject;
+    private SpriteRenderer pistolSprite;
+    private bool pistolEquipped = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         player = GetComponent<Rigidbody2D>();
         playerAnimator = GetComponent<Animator>();
         legPivot = GameObject.Find("Player/LegPivot");
+        armPivot = GameObject.Find("Player/ArmPivot");
         head = GameObject.Find("Player/Head");
-
+        pistolObject = GameObject.Find("ArmPivot/Pistol");
+        pistolSprite = pistolObject.GetComponent<SpriteRenderer>();
+        //Time.timeScale = 0.2f;
     }
 
     // Update is called once per frame
     void Update()
     {
         MovePlayer();
-        LookAtMouse();
-        SetWalkingAnimation();
+        WeaponHandle();
+        SetAnimation();
         // Move the Camera to the player
         Camera.main.transform.position = player.transform.position + cameraOffset;
+    }
+
+    private void WeaponHandle()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1)) // Switch to unarmed
+        {
+            //EquipUnarmed();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2)) // Switch to ranged weapon
+        {
+            //EquipRangedWeapon();
+        }
     }
 
     private void MovePlayer()
@@ -58,22 +81,22 @@ public class PlayerController : MonoBehaviour
         // Apply the movement to the Rigidbody2D velocity
         player.linearVelocity = new Vector2(playerMovement.x, playerMovement.y);
     }
-        void LookAtMouse()
+    private void LookAtMouse(GameObject Object)
     {
         // Get the mouse position
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
         // Calculate the direction from the sprite to the mouse
-        Vector2 direction = (mousePosition - head.transform.position).normalized;
+        Vector2 direction = (mousePosition - Object.transform.position).normalized;
 
         // Calculate the angle between the sprite's forward vector and the mouse
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90;
 
         // Apply the rotation to the head
-        head.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+        Object.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
     }
 
-    private void SetWalkingAnimation()
+    private void SetAnimation()
     {
         // Toggle the Animation Parameters based on whether the player is moving
         if (player.linearVelocity != Vector2.zero)
@@ -81,13 +104,47 @@ public class PlayerController : MonoBehaviour
             playerAnimator.SetBool("isWalking", true);
             playerAnimator.SetBool("isIdle", false);
             //set the walk angle based on the velocity direction
-            float walkAngle = Mathf.Atan2(player.linearVelocity.y, player.linearVelocity.x) * Mathf.Rad2Deg + 90;
+            float walkAngle = Mathf.Atan2(player.linearVelocity.y, player.linearVelocity.x) * Mathf.Rad2Deg + -90;
             legPivot.transform.rotation = Quaternion.Euler(new Vector3(0, 0, walkAngle));
+            
+
+        if(Input.GetMouseButtonDown(1))
+        {
+            LookAtMouse(armPivot);
+        }
+        else
+        {
+            armPivot.transform.rotation = Quaternion.Euler(new Vector3(0, 0, walkAngle));
+        }
+
+
         }
         else
         {
             playerAnimator.SetBool("isIdle", true);
             playerAnimator.SetBool("isWalking", false);
         }
+
+        if(Input.GetMouseButtonDown(1))
+        {
+            playerAnimator.SetBool("isAiming", true);
+            LookAtMouse(armPivot);
+        }
+        else if(Input.GetMouseButtonUp(1))
+        {
+            playerAnimator.SetBool("isAiming", false);
+        }
+
+        LookAtMouse(head);
+    }
+
+    public void PistolLayerAbove()
+    {
+        pistolSprite.sortingOrder = 3;
+    }
+
+    public void PistolLayerBelow()
+    {
+        pistolSprite.sortingOrder = 1;
     }
 }
